df866d5cea66e9cc8ab69efbc594fc0dded1707b
Teresa Ventaja <71657946+teresaventaja@users.noreply.github.com>
2023-12-10 14:06:43 +0000

    Initial commit

---
+++ src/gbbo_league/GBBO_League.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gbbo_league;
+
+/**
+ *
+ * @author User
+ */
+public class GBBO_League {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+/*  Based on points. Each week, players choose three contestants (their fantasy team)
+Data and history stored in csv “Fantasy GBBO.csv”
+        Players each week enter their predictions, actual results provided in csv file each week (player info and contest info)
+        Players should have: 
+        - see a list of players and their cumulative point so far. 
+        - See their own history of predictions and point scoring
+        
+        Weekly points awarded up to and including episode nine:
+        SELECTIONS: BEST BAKER, BAKER TO LEAVE, WINNER OF THAT WEEK'S TECHNICAL ROUND
+    Correct nomination of the Best Baker = 4 points
+    If your nominated Best Baker is eliminated, then 1 point will be deducted from your total
+    Correct weekly nomination of baker to leave = 3 points
+    If your nominated baker to leave is the Best Baker 1 point will be deducted from your total.
+    Correct nomination of the winner of that week’s technical round = 2 points
+ 
+Points awarded at the end of the final episode of the series:
+        SELECTIONS: OVERALL WINNER, 2 FINALISTS, WINNER FINAL EPISODE
+    Overall winner chosen after episode one and before episode two = 7 points
+    The other two finalists chosen after episode one and before episode two = 2 points each
+    Winner of the final episode, chosen after episode nine = 4 points
+    No technical round or elimination nominations for the final episode
+*/
+        
+    }
+    
+}
